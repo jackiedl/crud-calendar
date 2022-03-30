03bf2b666eefcb9837535d9d7397cefbda8393e0
Jackie Luu <luujackie98@gmail.com>
2022-03-29 22:11:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
