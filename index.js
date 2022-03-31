88eaa9c4cfee8ddba2627a978d7c2000cf8ebd36
Jackie Luu <luujackie98@gmail.com>
2022-03-30 21:57:52 -0700

    Initialize client using npx create-react-app

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
