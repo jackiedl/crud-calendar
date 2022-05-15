f90af20def1a388047c9695e2ecec80c7ab53fc8
Jackie <luujackie98@gmail.com>
2022-05-15 13:55:46 -0700

    Add update and delete routes

--- server/controllers/PostTaskController.js
+++ server/controllers/PostTaskController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostTask from "../models/PostTaskModel.js";
 
 export const getTasks = async (req, res) => {
@@ -27,9 +28,24 @@ export const createTasks = async (req, res) => {
 }
 
 export const updateTask = async (req, res) => {
-  
+  const { id: _id } = req.params;
+
+  const task = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No task with that id");
+
+  const updatedTask = await PostTask.findByIdAndUpdate(_id, task, {new: true});
+
+  res.json(updateTask);
+
 }
 
 export const deleteTask = async (req, res) => {
+  const { id: _id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No task with that id");
+
+  const deleteTask = await PostTask.findByIdAndRemove(_id);
+
+  res.json({message: "Task deleted sucessfully"});
 }
--- server/routes/PostTaskRoute.js
+++ server/routes/PostTaskRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { getTasks, createTasks } from "../controllers/PostTaskController.js";
+import { getTasks, createTasks, updateTask, deleteTask } from "../controllers/PostTaskController.js";
 
 const router = express.Router();
 
 router.get("/", getTasks);
 router.post("/", createTasks);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
