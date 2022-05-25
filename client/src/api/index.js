7a8af5c83894e6bde33dc52db2e8c6dcb3339a45
Jackie <luujackie98@gmail.com>
2022-05-24 18:28:43 -0700

    Fix api endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http:/https://jdl-crud-calendar.herokuapp.com/"})
+const API = axios.create({ baseURL: "https://jdl-crud-calendar.herokuapp.com/"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
