6dd7699398971544a5250ea13f19ef176138ccbe
Jackie <luujackie98@gmail.com>
2022-05-24 21:04:41 -0700

    Deploy client side to Firebase

--- client/src/components/copyright/Copyright.js
+++ client/src/components/copyright/Copyright.js
@@ -7,7 +7,7 @@ export default function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="/">
+      <Link color="inherit" href="https://github.com/jackiedl">
         CRUD Calendar
       </Link>
       {' '}
